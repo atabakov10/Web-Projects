9e2f424dc1f835104eccd1d9033674e41bc60eea
ANGEL <atabakov99@abv.bg>
2022-10-06 00:40:39 +0300

    Created PostController and View

---
+++ ForumApp/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using ForumApp.Data;
+using ForumApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ForumAppDbContext context;
+
+        public PostController(ForumAppDbContext _context)
+        {
+            context=_context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var model = await context.Posts
+                .Select(p=> new PostViewModel()
+                {
+                    Id=p.Id,
+                    Title = p.Title,
+                    Content = p.Content
+                }) 
+                .ToListAsync();  
+            return View();
+        }
+    }
+}
